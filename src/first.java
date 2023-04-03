781de911ac67af78f9e0feaa0560bc080b24130f
zbekxz <forestnatplay@yandex.ru>
2023-04-04 00:14:23 +0600

    number of fibonacci sequence

--- src/first.java
+++ src/first.java
@@ -32,9 +32,17 @@ public class first {
             return n * factorial(n-1);
         }
     }
+    static int fib(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        else {
+            return fib(n-1) + fib(n-2);
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println(factorial(n));
+        System.out.println(fib(n));
     }
 }
