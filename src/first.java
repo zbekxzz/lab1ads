3d6a0e1acb3e0a623d7b5cc537fcc2e3a213d0d3
zbekxz <forestnatplay@yandex.ru>
2023-04-03 23:39:24 +0600

    func that find a min number in array

---
+++ src/first.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class first {
+    static int minNum(int n, int[] nums) {
+        int min = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < min) {
+                min = nums[i];
+            }
+        }
+        return min;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(minNum(n, nums));
+    }
+}
