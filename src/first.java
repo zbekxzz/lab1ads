cae70ff7f98a62a5e1ae32122373c07166d6cfd0
zbekxz <forestnatplay@yandex.ru>
2023-04-03 23:58:54 +0600

    define number is prime

--- src/first.java
+++ src/first.java
@@ -17,13 +17,17 @@ public class first {
         }
         return total / n;
     }
+    static boolean isPrime(int n) {
+        int del = 0;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) { del++; }
+        }
+        return del == 1;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] nums = new int[n];
-        for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
-        }
-        System.out.println(avg(n, nums));
+        if (isPrime(n)) { System.out.println("Prime"); }
+        else { System.out.println("Composite"); }
     }
 }
