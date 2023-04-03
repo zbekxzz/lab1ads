08f966152092ab7c6a487a7bfeb5798654dfc9b5
zbekxz <forestnatplay@yandex.ru>
2023-04-04 00:11:23 +0600

    factorial of number function

--- src/first.java
+++ src/first.java
@@ -24,10 +24,17 @@ public class first {
         }
         return del == 1;
     }
+    static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        else {
+            return n * factorial(n-1);
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if (isPrime(n)) { System.out.println("Prime"); }
-        else { System.out.println("Composite"); }
+        System.out.println(factorial(n));
     }
 }
