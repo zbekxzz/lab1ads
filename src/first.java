7ac28fa1ec0306f39590526980242ec69af617e7
zbekxz <forestnatplay@yandex.ru>
2023-04-04 09:21:34 +0600

    find binomial coefficient by two numbers

--- src/first.java
+++ src/first.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class first {
-    static int minNum(int n, int[] nums) {
+    static int findMin(int n, int[] nums) {
         int min = nums[0];
         for (int i = 1; i < n; i++) {
             if (nums[i] < min) {
@@ -10,50 +10,50 @@ public class first {
         }
         return min;
     }
-    static double avg(int n, int[] nums) {
+    static double findAvg(int n, int[] nums) {
         double total = nums[0];
         for (int i = 1; i < n; i++) {
             total += nums[i];
         }
         return total / n;
     }
-    static boolean isPrime(int n) {
+    static boolean isDigitPrime(int n) {
         int del = 0;
         for (int i = 1; i < n; i++) {
             if (n % i == 0) { del++; }
         }
         return del == 1;
     }
-    static int factorial(int n) {
+    static int findFactorial(int n) {
         if (n == 1) {
             return 1;
         }
         else {
-            return n * factorial(n-1);
+            return n * findFactorial(n-1);
         }
     }
-    static int fib(int n) {
+    static int findFibonacci(int n) {
         if (n == 1 || n == 2) {
             return 1;
         }
         else {
-            return fib(n-1) + fib(n-2);
+            return findFibonacci(n-1) + findFibonacci(n-2);
         }
     }
-    static int power(int a, int n) {
+    static int findPower(int a, int n) {
         if (n == 1) {
             return a;
         }
         else {
-            return a * power(a, n-1);
+            return a * findPower(a, n-1);
         }
     }
-    static String reverse(int n, int[] nums) {
+    static String getReverse(int n, int[] nums) {
         if (n == 1) {
             return Integer.toString(nums[0]);
         }
         else {
-            return Integer.toString(nums[n-1]) + " " + reverse(n-1, nums);
+            return Integer.toString(nums[n-1]) + " " + getReverse(n-1, nums);
         }
     }
     static boolean isAllDigit(String line) {
@@ -64,9 +64,18 @@ public class first {
             return Character.isDigit(line.charAt(0)) && isAllDigit(line.substring(1));
         }
     }
+    static int findBinom(int n, int k) {
+        if (k == 0 || k == n) {
+            return 1;
+        }
+        else {
+            return findBinom(n-1, k-1) + findBinom(n-1, k);
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine();
-        System.out.println(isAllDigit(line));
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        System.out.println(findBinom(n,k));
     }
 }
